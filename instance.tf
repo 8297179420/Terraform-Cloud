49c221c0dbb47d5a7dcc52d902b7faa562a7792b
Pula Chaitra <109521614+8297179420@users.noreply.github.com>
2023-02-08 12:01:03 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key=var.AWS_ACCESS_KEY
-  secret_key=var.AWS_SECRET_KEY 
+  access_key=AKIA2UJTPUWYKECURBFA
+  secret_key=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
 }
 resource "aws_instance" "web" {
   ami           = "ami-01a4f99c4ac11b03c"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Terraform cloud12"
   }
 }
