f911b16a7475134a27213838bee5d7ae950ae83b
Pula Chaitra <109521614+8297179420@users.noreply.github.com>
2023-02-08 12:09:55 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key=AKIA2UJTPUWYKECURBFA
-  secret_key=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
+  access_key=var."AKIA2UJTPUWYKECURBFA"
+  secret_key=var."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"
 }
 resource "aws_instance" "web" {
   ami           = "ami-01a4f99c4ac11b03c"
