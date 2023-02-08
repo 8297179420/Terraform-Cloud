1cedf6b11c3644c3aeb94de64934fa3592c9cf67
Pula Chaitra <109521614+8297179420@users.noreply.github.com>
2023-02-08 10:40:40 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "sa-east-1"
+  region = "ap-south-1"
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY 
 }
 resource "aws_instance" "web" {
-  ami           = "ami-02dc8ad50da58fffd"
-  instance_type = "t3.micro"
+  ami           = "ami-01a4f99c4ac11b03c"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
